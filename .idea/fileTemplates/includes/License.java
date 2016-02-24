443959793da44ba531d1a93c7407f0dcc11dbf23
Victor Sonora Pombo <victor.pombo@mytechia.com>
2016-02-24 18:19:46 +0100

    Adds project for comm messages Java implementation.

---
+++ .idea/fileTemplates/includes/License.java
@@ -0,0 +1,5 @@
+#set( $author = "Victor Sonora Pombo <victor.pombo@mytechia.com>" )
+#set( $organization = "Mytech Ingenieria Aplicada <http://www.mytechia.com>" )
+#set( $project_name = "robobo-rob-interface" )
+
+#parse("simple_copyright_mytechia.java")
---
+++ src/main/java/com/mytechia/robobo/rob/comm/RobStatusMessage.java
@@ -0,0 +1,19 @@
+/**
+ * *****************************************************************************
+ * <p>
+ * Copyright (C) 2016 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ * Copyright (C) 2016 Victor Sonora Pombo <victor.pombo@mytechia.com>
+ * <p>
+ * This file is part of robobo-rob-interface.
+ * ****************************************************************************
+ */
+package com.mytechia.robobo.rob.comm;
+
+/**
+ * Description goes here. Please.
+ *
+ * Created by Victor Sonora Pombo <victor.pombo@mytechia.com>.
+ */
+public class RobStatusMessage {
+
+}
