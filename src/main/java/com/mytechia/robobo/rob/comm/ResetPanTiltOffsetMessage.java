77e0d6a24c0c0caffd2f4c7f6c9f722e5e465b23
Victor Sonora Pombo <victor.pombo@mytechia.com>
2016-02-25 16:55:32 +0100

    Añade implementación para ResetPanTiltOffsetMessage. (Hacer un test para esto, es tontería).

---
+++ src/main/java/com/mytechia/robobo/rob/comm/ResetPanTiltOffsetMessage.java
@@ -0,0 +1,35 @@
+/**
+ * *****************************************************************************
+ * <p>
+ * Copyright (C) 2016 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ * Copyright (C) 2016 Victor Sonora Pombo <victor.pombo@mytechia.com>
+ * <p>
+ * This file is part of robobo-rob-interface.
+ * ****************************************************************************
+ */
+package com.mytechia.robobo.rob.comm;
+
+import com.mytechia.commons.framework.simplemessageprotocol.Command;
+import com.mytechia.commons.framework.simplemessageprotocol.exception.MessageFormatException;
+
+/**
+ *
+ * Created by Victor Sonora Pombo <victor.pombo@mytechia.com>.
+ */
+public class ResetPanTiltOffsetMessage extends Command {
+
+
+    public ResetPanTiltOffsetMessage(byte mode) {
+        super();
+    }
+
+
+    public ResetPanTiltOffsetMessage(byte [] messageData) throws MessageFormatException {
+        super(messageData);
+    }
+
+    @Override
+    protected int decodeMessageData(byte[] bytes, int i) throws MessageFormatException {
+        return 0;
+    }
+}
